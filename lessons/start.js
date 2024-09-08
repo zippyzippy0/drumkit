3570f89ae2257d3b3b71359743fdb294d0e87ba3
unknown <LENOVO@Zipporah.ZIPPORAH>
2024-09-08 18:00:27 +0300

    my user interface

---
+++ lessons/start.js
@@ -0,0 +1,8 @@
+alert ("Hello WOrld!")
+//undefin ed
+let js = 'amazing'
+//undefined
+if (js === 'amazing') alert('Javascript is fun')
+// undefined
+"boring"
+if (js === 'amazing') alert('Javascript is fun')
